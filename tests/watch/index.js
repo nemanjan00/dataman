17d850b67377fa31d6016e64bf6e03cd7b1aecbc
nemanjan00 <nemanjan00@gmail.com>
2020-09-16 21:24:08 +0200

    Add watch tests

--- tests/watch/index.js
+++ tests/watch/index.js
@@ -14,5 +14,53 @@ describe("watch", function() {
 
 		return done();
 	});
+
+	it("Returns different value, if value is changed", function(done) {
+		const data = {
+			time: Date.now()
+		};
+
+		const watcher = watch(JSON.parse(JSON.stringify(data)));
+
+		watcher.time = 1;
+
+		if(watcher.time === data.time) {
+			return done(new Error("Data did not change"));
+		}
+
+		return done();
+	});
+
+	it("Remembers value changed", function(done) {
+		const data = {
+			time: Date.now()
+		};
+
+		const watcher = watch(JSON.parse(JSON.stringify(data)));
+
+		watcher.time = 1;
+
+		if(watcher.changes.time !== 1) {
+			return done(new Error("Did not remember change"));
+		}
+
+		return done();
+	});
+
+	it("Does nothing if new value is equal to the old one", function(done) {
+		const data = {
+			time: 1
+		};
+
+		const watcher = watch(JSON.parse(JSON.stringify(data)));
+
+		watcher.time = 1;
+
+		if(watcher.changes.time === 1) {
+			return done(new Error("Did not skip change"));
+		}
+
+		return done();
+	});
 });
 
