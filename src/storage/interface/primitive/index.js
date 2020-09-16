e1fe8bc4d5b849b758a388158598ea680b1f6f87
nemanjan00 <nemanjan00@gmail.com>
2020-09-16 23:00:38 +0200

    Primitive

--- src/storage/interface/primitive/index.js
+++ src/storage/interface/primitive/index.js
@@ -1,2 +1,22 @@
-module.exports = {};
+module.exports = {
+	/**
+	 * Set element value
+	 * @param {string} name Name
+	 * @param {string|number|boolean} element Element
+	 * @returns {Promise<boolean>}
+	 */
+	set: function(queueName, element) {
+		console.log("This is interface, do not call it", queueName, element);
+	},
+
+	/**
+	 * Get value
+	 * @param {string} name Name
+	 * @returns {Promise}
+	 */
+	get: function(queueName) {
+		console.log("This is interface, do not call it", queueName);
+	}
+};
+
 
